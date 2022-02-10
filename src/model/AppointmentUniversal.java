b39de9a4479593d6bc2a5e008043ded7407afaa1
Mike Wills <tamichealwills@gmail.com>
2022-02-10 05:17:56 -0600

    Complete edit of appointment and appointment universal

--- src/model/AppointmentUniversal.java
+++ src/model/AppointmentUniversal.java
@@ -7,9 +7,9 @@ package model;
 
 import java.sql.Timestamp;
 
-/**
- *
- * @author tamic
+/** This class represents the appointment universal model and is 
+ * used to create appointment objects. This class extends the
+ * appointment class.
  */
 public class AppointmentUniversal extends Appointment{
      public String formattedStartDateTime;
@@ -33,34 +33,53 @@ public class AppointmentUniversal extends Appointment{
         this.formattedUpdatedDateTime = formattedUpdatedDateTime;
     }
  
+    
+    /**
+     * @return Returns the formatted start date time
+     */   
     public String getFormattedStartDateTime() {
         return formattedStartDateTime;
     }
 
+    /** sets the formatted start date time*/
     public void setFormattedStartDateTime(String formattedStartDateTime) {
         this.formattedStartDateTime = formattedStartDateTime;
     }
-
+    
+    /**
+     * @return Returns the formatted end date time
+     */   
     public String getFormattedEndDateTime() {
         return formattedEndDateTime;
     }
 
+    /** sets the formatted end date time*/
     public void setFormattedEndDateTime(String formattedEndDateTime) {
         this.formattedEndDateTime = formattedEndDateTime;
     }
 
+    
+    /**
+     * @return Returns the formatted created date time
+     */   
     public String getFormattedCreatedDateTime() {
         return formattedCreatedDateTime;
     }
 
+    /** sets the formatted created date time*/
     public void setFormattedCreatedDateTime(String formattedCreatedDateTime) {
         this.formattedCreatedDateTime = formattedCreatedDateTime;
     }
 
+    
+    /**
+     * @return Returns the formatted up date time
+     */   
     public String getFormattedUpdatedDateTime() {
         return formattedUpdatedDateTime;
     }
-
+    
+    /** sets the formatted up date time*/
     public void setFormattedUpdatedDateTime(String formattedUpdatedDateTime) {
         this.formattedUpdatedDateTime = formattedUpdatedDateTime;
     }
